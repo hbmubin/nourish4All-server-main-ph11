7aa330accc61b133a43fd04d36c1960c32081baf
hbmubin <hasanulbannamubin01@gmail.com>
2024-05-13 19:42:08 +0600

    delete-food-done

--- index.js
+++ index.js
@@ -63,6 +63,12 @@ async function run() {
       const result = await foodCollection.find(query).toArray();
       res.send(result);
     });
+    app.delete("/foods/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
